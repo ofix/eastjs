c0d3a3e53c35e15f47f533d00d31784ba94e4432
ofix <shb8845369@gmail.com>
2020-10-07 16:45:21 +0800

    增加 PdfPrinter 页面

---
+++ src/renderer/components/PdfPrinter/PdfPrinter.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-container>
+    <el-aside width="200px"> 左侧菜单 </el-aside>
+    <el-container>
+      <el-main>中间内容</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "pdf-printer",
+  methods: {
+    open(link) {
+      this.$electron.shell.openExternal(link);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+</style>
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -5,10 +5,15 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'landing-page',
+    //   component: require('@/components/LandingPage').default
+    // },
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
+      name: 'pdf-printer',
+      component: require('@/components/PdfPrinter/PdfPrinter').default
     },
     {
       path: '*',
